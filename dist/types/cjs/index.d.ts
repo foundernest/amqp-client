7c7d5946938423da4f4efc5e44187b7ca067fdec
jaume-foundernest <jaume@foundernest.com>
2025-01-15 16:14:21 +0100

    :package: Update dist

--- dist/types/cjs/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './amqp-client';
-export * from './amqp-client.interface';
-export * from './amqp-client.types';
-export * from './amqp-client-logger.interface';
--- dist/types/esm/amqp-client-logger.interface.d.ts
+++
@@ -1,6 +0,0 @@
-export interface AMQPClientLoggerInterface {
-    debug(message: string, ...args: any[]): void;
-    info(message: string, ...args: any[]): void;
-    warn(message: string, ...args: any[]): void;
-    error(message: string, ...args: any[]): void;
-}
