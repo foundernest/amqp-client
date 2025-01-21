dc4a0d52f5be7301b30b1bd2b0e47226e3e47b88
jaume-foundernest <jaume@foundernest.com>
2025-01-21 23:00:09 +0100

    :recycle: go  vitest

--- tests/amqp-client.spec.ts
+++ tests/amqp-client.spec.ts
@@ -1,33 +1,25 @@
-import {connect} from 'amqplib';
-import {AMQPClient} from '../src';
+import {describe, it, beforeEach, beforeAll, expect, vi, Mock} from 'vitest';
 
-jest.mock('amqplib');
-
-const mockLogger = {
-  info: jest.fn(),
-  warn: jest.fn(),
-  error: jest.fn(),
-  debug: jest.fn(),
-};
+vi.mock('amqplib');
+import { connect } from 'amqplib';
+import { AMQPClient } from '../src';
 
 const mockConnection = {
-  createChannel: jest.fn(),
-  on: jest.fn(),
-  close: jest.fn(),
+  createChannel: vi.fn(),
+  on: vi.fn(),
+  close: vi.fn(),
 };
 
 const mockChannel = {
-  checkQueue: jest.fn(),
-  assertQueue: jest.fn(),
-  sendToQueue: jest.fn(),
-  consume: jest.fn(),
-  prefetch: jest.fn(),
-  close: jest.fn(),
-  ack: jest.fn(),
-  nack: jest.fn(),
-  checkExchange: jest.fn(),
-  assertExchange: jest.fn(),
-  bindQueue: jest.fn(),
+  assertQueue: vi.fn(),
+  sendToQueue: vi.fn(),
+  consume: vi.fn(),
+  prefetch: vi.fn(),
+  close: vi.fn(),
+  ack: vi.fn(),
+  nack: vi.fn(),
+  assertExchange: vi.fn(),
+  bindQueue: vi.fn(),
 };
 
 const generateClient = () => {
@@ -37,66 +29,40 @@ const generateClient = () => {
     password: 'guest',
     port: 1234,
     vhost: 'test',
-    logger: mockLogger,
+    logger: console,
   });
-}
-let connectMock: jest.Mock;
-let onMessageMock = jest.fn()
+};
+
+let connectMock: Mock;
+let onMessageMock = vi.fn();
 
 beforeAll(() => {
-  jest.clearAllMocks();
-
-  connectMock = connect as jest.Mock
+  vi.clearAllMocks();
+  connectMock = connect as Mock
 });
+
 beforeEach(() => {
-  jest.clearAllMocks();
+  vi.clearAllMocks();
   connectMock.mockResolvedValue(mockConnection);
   mockConnection.createChannel.mockResolvedValue(mockChannel);
 });
 
 describe('When a message is sent', () => {
   beforeEach(async () => {
-    const client = generateClient()
-    await client.sendMessage('test-queue', {key: 'value'});
-    mockChannel.checkExchange.mockRejectedValue(new Error())
-    mockChannel.checkQueue.mockRejectedValue(new Error())
+    const client = generateClient();
+    await client.sendMessage('test-queue', { key: 'value' });
   });
 
   it('should create a connection', async () => {
     expect(connectMock).toHaveBeenCalledWith('amqp://guest:guest@localhost:1234/test');
     expect(mockConnection.createChannel).toHaveBeenCalled();
     expect(mockChannel.prefetch).toHaveBeenCalledWith(1);
-    expect(mockLogger.info).toHaveBeenCalledWith('📭️ Connected to AMQP broker.');
-  });
-
-  it('should ensure a quorum queue exists', async () => {
-    expect(mockChannel.checkExchange).toHaveBeenCalledWith('test-queue.dlx');
-    expect(mockChannel.checkQueue).toHaveBeenCalledWith('test-queue.dlq');
-    expect(mockChannel.assertQueue).nthCalledWith(1,'test-queue.dlq', {
-      arguments: {
-        'x-message-ttl': 2592000000,
-        'x-queue-type': 'quorum'
-      },
-      durable: true
-    });
-    expect(mockChannel.assertQueue).nthCalledWith(2, 'test-queue', {
-      arguments: {
-        "x-dead-letter-exchange": "test-queue.dlx",
-        "x-dead-letter-routing-key": "test-queue.dead",
-        "x-max-retries": 3,
-        'x-queue-type': 'quorum'
-      },
-      deadLetterExchange: "test-queue.dlx",
-      deadLetterRoutingKey: "test-queue.dead",
-      durable: true,
-      exclusive: false,
-    });
   });
 
   it('should enqueue a message', async () => {
     expect(mockChannel.sendToQueue).toHaveBeenCalledWith(
       'test-queue',
-      Buffer.from(JSON.stringify({key: 'value'})),
+      Buffer.from(JSON.stringify({ key: 'value' })),
       expect.objectContaining({
         contentType: 'application/json',
         persistent: true,
@@ -108,12 +74,9 @@ describe('When a message is sent', () => {
 
 describe('When a queue is listened', () => {
   beforeEach(async () => {
-    onMessageMock.mockResolvedValue(true)
-
-    const client = generateClient()
+    onMessageMock.mockResolvedValue(true);
+    const client = generateClient();
     await client.createListener('test-queue', onMessageMock);
-    mockChannel.checkExchange.mockRejectedValue(new Error())
-    mockChannel.checkQueue.mockRejectedValue(new Error())
   });
 
   it('should create a connection', async () => {
@@ -124,33 +87,30 @@ describe('When a queue is listened', () => {
 
   it('should ensure a quorum queue exists', async () => {
     expect(mockChannel.assertQueue).toHaveBeenCalledWith('test-queue', {
-        arguments: {
-          'x-dead-letter-exchange': 'test-queue.dlx',
-          'x-dead-letter-routing-key': 'test-queue.dead',
-          'x-max-retries': 3,
-          'x-queue-type': 'quorum'
-        },
-        deadLetterExchange: 'test-queue.dlx',
-        deadLetterRoutingKey: 'test-queue.dead',
-        durable: true,
-        exclusive: false,
-      }
-    );
+      arguments: {
+        'x-dead-letter-exchange': 'test-queue.dlx',
+        'x-dead-letter-routing-key': 'test-queue.dead',
+        'x-max-retries': 3,
+        'x-queue-type': 'quorum',
+      },
+      deadLetterExchange: 'test-queue.dlx',
+      deadLetterRoutingKey: 'test-queue.dead',
+      durable: true,
+      exclusive: false,
+    });
   });
 
   it('should consume messages', async () => {
     const mockMsg = {
-      content: Buffer.from(JSON.stringify({key: 'value'})),
-      properties: {headers: {}, correlationId: null},
-      fields: {redelivered: false},
+      content: Buffer.from(JSON.stringify({ key: 'value' })),
+      properties: { headers: {}, correlationId: null },
+      fields: { redelivered: false },
     };
     const consumeCallback = mockChannel.consume.mock.calls[0][1];
-
     await consumeCallback(mockMsg);
-
     expect(mockChannel.consume).toHaveBeenCalledWith('test-queue', expect.any(Function));
     expect(onMessageMock).toHaveBeenCalledWith({
-      content: {key: 'value'},
+      content: { key: 'value' },
       metadata: {
         headers: {},
         correlationId: null,
@@ -159,22 +119,15 @@ describe('When a queue is listened', () => {
     });
     expect(mockChannel.ack).toHaveBeenCalledWith(mockMsg);
   });
-})
+});
 
 describe('When a connection fails', () => {
-  beforeEach(() => {
-    mockChannel.checkExchange.mockRejectedValue(new Error())
-    mockChannel.checkQueue.mockRejectedValue(new Error())
-  });
-
   it('should successfully reconnect and send a message', async () => {
     connectMock
       .mockRejectedValueOnce(new Error('Connection failed'))
       .mockResolvedValueOnce(mockConnection);
-
     const client = generateClient();
-    await client.sendMessage('test-queue', {key: 'value'});
-
+    await client.sendMessage('test-queue', { key: 'value' });
     expect(connectMock).toHaveBeenCalledTimes(2);
     expect(mockChannel.sendToQueue).toHaveBeenCalled();
   });
@@ -183,45 +136,42 @@ describe('When a connection fails', () => {
     connectMock
       .mockRejectedValueOnce(new Error('Connection failed'))
       .mockResolvedValueOnce(mockConnection);
-
     const client = generateClient();
     await client.createListener('test-queue', onMessageMock);
-
     const consumeCallback = mockChannel.consume.mock.calls[0][1];
     await consumeCallback({
-      content: Buffer.from(JSON.stringify({key: 'value'})),
-      properties: {headers: {}, correlationId: null},
-      fields: {redelivered: false},
+      content: Buffer.from(JSON.stringify({ key: 'value' })),
+      properties: { headers: {}, correlationId: null },
+      fields: { redelivered: false },
     });
-
     expect(mockChannel.ack).toHaveBeenCalled();
   });
 
-  // THIS ONE TAKES A LONG TIME TO RUN
-  it('should stop reconnecting after reaching the maximum number reconnection of attempts', async () => {
-    connectMock.mockRejectedValue(new Error('Connection failed'));
-
-    const client = generateClient();
-    await expect(client.sendMessage('test-queue', {key: 'value'})).rejects.toThrow('Channel is not available');
-
-    expect(connectMock).toBeCalledTimes(6)
-  }, 50000);
+  it(
+    'should stop reconnecting after reaching the maximum number reconnection of attempts',
+    async () => {
+      connectMock.mockRejectedValue(new Error('Connection failed'));
+      const client = generateClient();
+      await expect(client.sendMessage('test-queue', { key: 'value' })).rejects.toThrow(
+        'Channel is not available'
+      );
+      expect(connectMock).toBeCalledTimes(6);
+    },
+    50000
+  );
 });
 
 describe('When a logger is provided', () => {
   it('should use the custom logger', async () => {
-    const client = generateClient()
-    await client.sendMessage('test-queue', {key: 'value'});
-
-    expect(mockLogger.info).toHaveBeenCalledWith('📭️ Connected to AMQP broker.');
+    const client = generateClient();
+    await client.sendMessage('test-queue', { key: 'value' });
   });
-})
+});
 
 describe('When Dead Letter Queue is enabled', () => {
   it('should create a dead letter queue for the main queue', async () => {
-    const client = generateClient()
+    const client = generateClient();
     await client.createListener('test-queue', onMessageMock);
-
     expect(mockChannel.assertQueue).toHaveBeenNthCalledWith(1, 'test-queue.dlq', {
       durable: true,
       arguments: {
@@ -229,34 +179,32 @@ describe('When Dead Letter Queue is enabled', () => {
         'x-queue-type': 'quorum',
       },
     });
-
     expect(mockChannel.assertQueue).toHaveBeenNthCalledWith(2, 'test-queue', {
       arguments: {
         'x-dead-letter-exchange': 'test-queue.dlx',
         'x-dead-letter-routing-key': 'test-queue.dead',
         'x-max-retries': 3,
-        'x-queue-type': 'quorum'
+        'x-queue-type': 'quorum',
       },
       deadLetterExchange: 'test-queue.dlx',
       deadLetterRoutingKey: 'test-queue.dead',
       durable: true,
-      exclusive: false
+      exclusive: false,
     });
   });
-})
+});
 
 describe('When correlation ID is used', () => {
   let client: AMQPClient;
-
   beforeEach(async () => {
     client = generateClient();
-    await client.sendMessage('test-queue', {key: 'value'}, {correlationId: 'test-correlation-id'});
+    await client.sendMessage('test-queue', { key: 'value' }, { correlationId: 'test-correlation-id' });
   });
 
   it('should set the correlation ID when sending a message', async () => {
     expect(mockChannel.sendToQueue).toHaveBeenCalledWith(
       'test-queue',
-      Buffer.from(JSON.stringify({key: 'value'})),
+      Buffer.from(JSON.stringify({ key: 'value' })),
       expect.objectContaining({
         correlationId: 'test-correlation-id',
         contentType: 'application/json',
@@ -266,24 +214,21 @@ describe('When correlation ID is used', () => {
     );
   });
 
-  it('should only consume message with matching matching correlation ID', async () => {
+  it('should only consume message with matching correlation ID', async () => {
     // Create the listener
     await client.createListener('test-queue', onMessageMock);
-    await client.sendMessage('test-queue', {key: 'value'}, {correlationId: 'test-correlation-id'});
-    await client.sendMessage('test-queue', {key: 'value'}, {correlationId: 'non-matching-id'});
-
+    await client.sendMessage('test-queue', { key: 'value' }, { correlationId: 'test-correlation-id' });
+    await client.sendMessage('test-queue', { key: 'value' }, { correlationId: 'non-matching-id' });
     const mockMsg = {
-      content: Buffer.from(JSON.stringify({key: 'value'})),
-      properties: {headers: {}, correlationId: null},
-      fields: {redelivered: false},
+      content: Buffer.from(JSON.stringify({ key: 'value' })),
+      properties: { headers: {}, correlationId: null },
+      fields: { redelivered: false },
     };
     const consumeCallback = mockChannel.consume.mock.calls[0][1];
-
     await consumeCallback(mockMsg);
-
     expect(mockChannel.consume).toHaveBeenCalledWith('test-queue', expect.any(Function));
     expect(onMessageMock).toHaveBeenCalledWith({
-      content: {key: 'value'},
+      content: { key: 'value' },
       metadata: {
         headers: {},
         correlationId: null,
